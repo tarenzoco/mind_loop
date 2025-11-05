2279a80741ef09220e707a4f044dbaca6b5ccc46
Tarenzo <tarenzo.co@gmail.com>
2025-11-05 21:58:56 +0530

    fix: switch to Babel build for cross-platform deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: false, // disable SWC (causing Linux build issues)
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
